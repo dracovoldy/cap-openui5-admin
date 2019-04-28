8b8de3d48d084c751b352b23c55d8200f1971358
dracovoldy <dracovoldy@gmail.com>
2019-04-28 13:21:40 +0000

    home filer

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -2,8 +2,10 @@ sap.ui.define([
 	'jquery.sap.global',
 	'sap/ui/core/mvc/Controller',
 	'sap/m/Popover',
-	'sap/m/Button'
-], function (jQuery, Controller, Popover, Button) {
+	'sap/m/Button',
+	'sap/ui/model/json/JSONModel',
+	'sap/ui/model/Filter'
+], function (jQuery, Controller, Popover, Button, JSONModel, Filter) {
 	"use strict";
 
 	return Controller.extend("cap.estimate.controller.Home", {
@@ -15,7 +17,7 @@ sap.ui.define([
 
 			var globalModel = this.getOwnerComponent().getModel("init_data");
 			this.getView().setModel(globalModel);
-			
+
 			var url = "http://10.154.52.73:3000/api/estimate";
 
 			$.ajax({
@@ -33,9 +35,72 @@ sap.ui.define([
 			});
 
 		},
-		loadRecord: function(oEvent){
+		loadRecord: function (oEvent) {
 			this.getView().getModel().setProperty("/posting", oEvent.getSource().getBindingContext().getObject());
 			this.router.navTo("Customer");
+		},
+		GoPress: function (oEvent) {
+			var CompanyName = this.getView().byId("name").getValue().trim();
+			var SectorValue = "";
+			if (this.getView().byId("Sector_Select").getSelectedKey() === "X") {
+				SectorValue = "";
+			} else {
+				SectorValue = this.getView().byId("Sector_Select").getSelectedKey();
+			}
+			var RegionValue = "";
+			if (this.getView().byId("Region_Select").getSelectedKey() === "X") {
+				RegionValue = "";
+			} else {
+				RegionValue = this.getView().byId("Region_Select").getSelectedKey();
+			}
+			var ClientValue = this.getView().byId("client_name").getValue().trim();
+			var CapgValue = this.getView().byId("capg_contact").getValue().trim();
+
+			var oFilter = [];
+			var andFilter = [];
+
+			if (CompanyName.length > 0) {
+				var filterKey = new Filter(
+					"comp_name",
+					sap.ui.model.FilterOperator.Contains, CompanyName
+				);
+				oFilter.push(filterKey);
+			}
+
+			if (SectorValue.length > 0) {
+				var filterKey = new Filter(
+					"comp_sector",
+					sap.ui.model.FilterOperator.EQ, SectorValue
+				);
+				oFilter.push(filterKey);
+			}
+			
+			if (RegionValue.length > 0) {
+				var filterKey = new Filter(
+					"comp_region",
+					sap.ui.model.FilterOperator.EQ, RegionValue
+				);
+				oFilter.push(filterKey);
+			}
+			
+			if (ClientValue.length > 0) {
+				var filterKey = new Filter(
+					"client_name",
+					sap.ui.model.FilterOperator.Contains, ClientValue
+				);
+				oFilter.push(filterKey);
+			}
+			
+			if (CapgValue.length > 0) {
+				var filterKey = new Filter(
+					"client_contact",
+					sap.ui.model.FilterOperator.Contains, CapgValue
+				);
+				oFilter.push(filterKey);
+			}
+			
+			// andFilter.push(new sap.ui.model.Filter(oFilter, false));
+			this.getView().byId("idProductsTable").getBinding("items").filter(new sap.ui.model.Filter(oFilter, true));
 		}
 
 	});
